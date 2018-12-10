82007a84773b82bfa02ac2793d585e62ebad7839
enes arican <enesarican@hotmail.com>
2018-12-10 22:24:00 +0000

    Initial commit

---
+++ FIleConverter/PrayerTime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIleConverter
+{
+    public class PrayerTime
+    {
+        public DateTime tarih { get; set; }
+
+        public string isani { get; set; }
+
+        public string asani { get; set; }
+
+        public string imsak { get; set; }
+
+        public string ogle { get; set; }
+
+        public string sabah { get; set; }
+
+        public string ikindi { get; set; }
+
+        public string kible { get; set; }
+
+        public string aksam { get; set; }
+
+        public string yatsi { get; set; }
+
+        public int bolge_id { get; set; }
+
+        public string gunes { get; set; }
+
+        public string israk { get; set; }
+
+    }
+}
---
+++ FIleConverter/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIleConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the File Converter App for Fazilet prayer times!");
+           
+            Console.WriteLine("Please Enter file path of .txt file that is in json format");
+
+            Console.ReadLine();
+
+            string path = @"C:\Users\enes\Documents\fazilet-takvimi-namaz-vakitleri\2019-namaz-vakitleri.txt";
+
+            using (StreamReader reader = new StreamReader(new FileStream(path, FileMode.Open)))
+            {
+
+                IEnumerable<PrayerTime> prayerTimes = JsonConvert.DeserializeObject<IEnumerable<PrayerTime>>(reader.ReadToEnd());
+
+
+                foreach (PrayerTime obj in prayerTimes)
+                {
+                    Console.WriteLine(obj.isani);
+                }
+
+            }
+
+            Console.WriteLine("Done Blud");
+            Console.ReadLine();
+
+        }
+    }
+}
