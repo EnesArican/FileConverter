c16d1262be09b58896a605538d52fb5484b972f6
Enes.Arican <Enes.Arican@investmentsoftwareltd.com>
2018-12-18 14:19:37 +0000

    working on looping through each day, month and year.

--- FIleConverter/ConverterTypes/AwaConverter.cs
+++ FIleConverter/ConverterTypes/AwaConverter.cs
@@ -12,37 +12,70 @@ namespace FileConverter.ConverterTypes
             SetHeader(file);
 
 
-            var year = prayerTimes.Min(p => p.Tarih.Year);
-            var prayerTimesForYear = prayerTimes.Where(p => p.Tarih.Year == year);
 
-            var month = prayerTimesForYear.Min(p => p.Tarih.Month);
-            var prayerTimesForMonth = prayerTimesForYear.Where(p => p.Tarih.Month == month);
-
-
-            var minHourInMonthSabah = prayerTimesForMonth.Min(p => p.Sabah.Hours);
-            var minHourInMonthGunes = prayerTimesForMonth.Min(p => p.Gunes.Hours);
-            var minHourInMonthOgle = prayerTimesForMonth.Min(p => p.Ogle.Hours);
-            var minHourInMonthIkindi = prayerTimesForMonth.Min(p => p.Ikindi.Hours);
-            var minHourInMonthAksam = prayerTimesForMonth.Min(p => p.Aksam.Hours);
-            var minHourInMonthYatsi = prayerTimesForMonth.Min(p => p.YatsiTime.Hours);
-
-
-            file.WriteLine(year);
-            file.WriteLine(minHourInMonthSabah + " " + minHourInMonthGunes + " " + minHourInMonthOgle +  " " + 
-                                                minHourInMonthIkindi + " " + minHourInMonthAksam + " " + minHourInMonthYatsi);
-
-            foreach (PrayerTime prayerTime in prayerTimesForMonth)
+            foreach (IEnumerable<PrayerTime> timesForYear in prayerTimes.GroupBy(p => p.Tarih.Year))
             {
-                var sabahMinutes = prayerTime.Sabah.TotalMinutes - (minHourInMonthSabah * 60);
-                var gunesMinutes = prayerTime.Gunes.TotalMinutes - (minHourInMonthGunes * 60);
-                var ogleMinutes = prayerTime.Ogle.TotalMinutes - (minHourInMonthOgle * 60);
-                var ikindiMinutes = prayerTime.Ikindi.TotalMinutes - (minHourInMonthIkindi * 60);
-                var aksamMinutes = prayerTime.Aksam.TotalMinutes - (minHourInMonthAksam * 60);
-                var yatsiMinutes = prayerTime.YatsiTime.TotalMinutes - (minHourInMonthYatsi * 60);
+                var year = timesForYear.Select(t => t.Tarih.Year).First();
+                file.WriteLine(year);
 
-                file.WriteLine(sabahMinutes + " " + gunesMinutes + " " + ogleMinutes + " " + ikindiMinutes + " " + aksamMinutes + " " + yatsiMinutes);
+                foreach (IEnumerable<PrayerTime> timesForMonth in timesForYear.GroupBy(p => p.Tarih.Month))
+                {
 
+                    var minHourInMonthSabah = timesForMonth.Min(p => p.Sabah.Hours);
+                    var minHourInMonthGunes = timesForMonth.Min(p => p.Gunes.Hours);
+                    var minHourInMonthOgle = timesForMonth.Min(p => p.Ogle.Hours);
+                    var minHourInMonthIkindi = timesForMonth.Min(p => p.Ikindi.Hours);
+                    var minHourInMonthAksam = timesForMonth.Min(p => p.Aksam.Hours);
+                    var minHourInMonthYatsi = timesForMonth.Min(p => p.YatsiTime.Hours);
+
+                    file.WriteLine(minHourInMonthSabah + " " + minHourInMonthGunes + " " + minHourInMonthOgle + " " +
+                                               minHourInMonthIkindi + " " + minHourInMonthAksam + " " + minHourInMonthYatsi);
+
+                    foreach (PrayerTime prayerTime in timesForMonth)
+                    {
+                        var sabahMinutes = prayerTime.Sabah.TotalMinutes - (minHourInMonthSabah * 60);
+                        var gunesMinutes = prayerTime.Gunes.TotalMinutes - (minHourInMonthGunes * 60);
+                        var ogleMinutes = prayerTime.Ogle.TotalMinutes - (minHourInMonthOgle * 60);
+                        var ikindiMinutes = prayerTime.Ikindi.TotalMinutes - (minHourInMonthIkindi * 60);
+                        var aksamMinutes = prayerTime.Aksam.TotalMinutes - (minHourInMonthAksam * 60);
+                        var yatsiMinutes = prayerTime.YatsiTime.TotalMinutes - (minHourInMonthYatsi * 60);
+
+                        file.WriteLine(sabahMinutes + " " + gunesMinutes + " " + ogleMinutes + " " + ikindiMinutes + " " + aksamMinutes + " " + yatsiMinutes);
+                    }
+                }
             }
+
+
+            // var year = prayerTimes.Min(p => p.Tarih.Year);
+            var prayerTimesForYear = prayerTimes;//.Where(p => p.Tarih.Year == year);
+
+            //var month = prayerTimesForYear.Min(p => p.Tarih.Month);
+            //var prayerTimesForMonth = prayerTimesForYear.Where(p => p.Tarih.Month == month);
+
+            //var minHourInMonthSabah = prayerTimesForMonth.Min(p => p.Sabah.Hours);
+            //var minHourInMonthGunes = prayerTimesForMonth.Min(p => p.Gunes.Hours);
+            //var minHourInMonthOgle = prayerTimesForMonth.Min(p => p.Ogle.Hours);
+            //var minHourInMonthIkindi = prayerTimesForMonth.Min(p => p.Ikindi.Hours);
+            //var minHourInMonthAksam = prayerTimesForMonth.Min(p => p.Aksam.Hours);
+            //var minHourInMonthYatsi = prayerTimesForMonth.Min(p => p.YatsiTime.Hours);
+
+
+            //file.WriteLine(year);
+            //file.WriteLine(minHourInMonthSabah + " " + minHourInMonthGunes + " " + minHourInMonthOgle +  " " + 
+            //                                    minHourInMonthIkindi + " " + minHourInMonthAksam + " " + minHourInMonthYatsi);
+
+            //foreach (PrayerTime prayerTime in prayerTimesForMonth)
+            //{
+            //    var sabahMinutes = prayerTime.Sabah.TotalMinutes - (minHourInMonthSabah * 60);
+            //    var gunesMinutes = prayerTime.Gunes.TotalMinutes - (minHourInMonthGunes * 60);
+            //    var ogleMinutes = prayerTime.Ogle.TotalMinutes - (minHourInMonthOgle * 60);
+            //    var ikindiMinutes = prayerTime.Ikindi.TotalMinutes - (minHourInMonthIkindi * 60);
+            //    var aksamMinutes = prayerTime.Aksam.TotalMinutes - (minHourInMonthAksam * 60);
+            //    var yatsiMinutes = prayerTime.YatsiTime.TotalMinutes - (minHourInMonthYatsi * 60);
+
+            //    file.WriteLine(sabahMinutes + " " + gunesMinutes + " " + ogleMinutes + " " + ikindiMinutes + " " + aksamMinutes + " " + yatsiMinutes);
+
+            //}
 
             
 
